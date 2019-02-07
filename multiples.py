7fd7ddfde2d4e1b3a540b95beb6eb343c8db9706
irinaBaB <44655446+irinaBaB@users.noreply.github.com>
2019-02-07 22:57:15 +1100

    adding project

---
+++ multiples.py
@@ -0,0 +1,25 @@
+
+def sum_elements(limit):
+    acum = 0
+    for i in range(limit):
+        if good(i):
+            acum = acum + i
+    return acum
+
+
+def good(i):
+    if i == 0:
+        return False
+    by_3 = (i % 3) == 0
+    by_5 = (i % 5) == 0
+    return by_3 or by_5
+
+
+# Task2:
+# for all numbers below 10 (excluding 0)
+# result of multiplication of all even numbers is 384=2*4*6*8
+# calculate the multiplication result of all even numbers below 100000
+
+
+# Task 3:
+# how to change sum_elements with list comprehension and map function
